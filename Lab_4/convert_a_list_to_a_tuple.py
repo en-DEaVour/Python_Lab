a0445371232883392db9b970cac00f6c302fed54
Mohd Mustajab Khan <meet2mustajabkhan@gmail.com>
2021-03-04 00:35:04 +0530

    Add files via upload

---
+++ Lab_4/convert_a_list_to_a_tuple.py
@@ -0,0 +1,13 @@
+#Name- MOHD MUSTAJAB KHAN
+#Roll No:- 33
+#En No:- A180570
+list= [5, 10, 7, 4, 15, 3]
+print("The given list is-:",list)
+def convert(list):
+ return tuple(list)
+print("After converting List to Tuple\nThe Output is-:")
+print(convert(list))
+
+
+#list = [1,2,3,4,5]
+#print(tuple(list))
